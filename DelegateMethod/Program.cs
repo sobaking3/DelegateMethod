9fc1ba5bec657f6697f23e6297d03fee7c2131fa
Мурад Рагимов <ragimovu1@tk34.mskobr.ru>
2022-04-11 09:46:09 +0300

    Осмыслил

---
+++ DelegateMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DelegateMethod
+{
+    public delegate string Hello(string name);
+    class SomeClass
+    {
+        public string SayHello(string s)
+        {
+            return $"Привет, {s}:";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SomeClass someClass = new SomeClass();
+            Hello hD = new Hello(someClass.SayHello);
+            Console.WriteLine(hD("Медведь"));
+            Console.ReadKey();
+        }
+    }
+}
